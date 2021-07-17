3c945d5e46cece64077a284b5fddfe07b44d4707
heysaeid <heysaeid92@gmail.com>
2021-07-17 14:52:41 +0430

    Complete the order creation and payment section

--- orders/forms.py
+++ orders/forms.py
@@ -2,14 +2,20 @@ from django import forms
 from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password', 'class':'input-text'}))
+
     class Meta:
         model = Order
-        fields =  ['first_name', 'last_name', 'address', 'city', 'county', 'postal_code', 'phone']
+        fields =  ['first_name', 'last_name', 'address', 'city', 'county', 'postal_code', 'phone', 'email', 'password']
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        self.request = kwargs.pop('request', None)
+        super(OrderCreateForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'input-text'
+
+        if self.request.user.is_authenticated:
+            self.fields['password'].required = False
 
         self.fields['city'].widget.attrs['placeholder'] = 'Town / City'
         self.fields['county'].widget.attrs['placeholder'] = 'State / County'
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import authenticate, login as auth_login
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.conf import settings
+from django.views.generic.base import TemplateResponseMixin, View
 import weasyprint
 from .models import Order, OrderItem
 from .forms import OrderCreateForm
@@ -11,34 +13,65 @@ from cart.cart import Cart
 from shop.models import Product
 from .task import order_created
 from shop.recommender import Recommender
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
-def order_create(request):
-    cart = Cart(request)
-    login_form = LoginForm()
-    if cart:
-        r = Recommender()
-        cart_products = [item['product'] for item in cart]
-        r.products_bought(cart_products)
-        if request.method == 'POST':
-            form = OrderCreateForm(request.POST)
-            if form.is_valid():
-                order = form.save(commit=False)
-                order.final_price = int(cart.get_total_price_after_discount()) * 23000
-                order.save()
-                for item in cart:
-                    OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-                cart.clear()
-                # launch asynchronous task
-                order_created.delay(order.id)
-                request.session['order_id'] = order.id
-                return redirect(reverse('payment:request'))
+class OrderCreateView(TemplateResponseMixin, View):
+    template_name = 'orders/order/create.html'
+    cart = None
+    login_form = None
+    error_message = None
+
+    def get_form(instance=None, data=None):
+        return OrderCreateForm(instance=instance, data=data)
+
+    def dispatch(self, request):
+        self.cart = Cart(request)
+        self.login_form = LoginForm()
+        return super().dispatch(request)
+
+    def get(self, request, *args, **kwargs):
+        if self.cart:
+            cart_products = [item['product'] for item in self.cart]
+            if request.user.is_authenticated:
+                form = OrderCreateForm(instance=request.user.orders.first(), request=request)
+            else:
+                form = OrderCreateForm(request=request)
+            return self.render_to_response({'cart':self.cart, 'form':form, 'login_form':self.login_form, 'error_message':self.error_message})
         else:
-            form = OrderCreateForm(instance=request.user) if request.user.is_authenticated else OrderCreateForm()
-        return render(request, 'orders/order/create.html', {'cart':cart, 'form':form, 'login_form':login_form})
-    else:
-        return redirect('cart:cart_detail')
+            return redirect('cart:cart_detail')
+
+    def post(self, request, *args, **kwargs):
+        form = OrderCreateForm(request.POST, request=request)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if not request.user.is_authenticated:
+                cd = form.cleaned_data
+                ind = cd['email'].index('@')
+                username = cd['email'][0:ind]
+                data = {'username':username, 'email':cd['email'], 'password1':cd['password'], 'password2':cd['password']}
+                register_form = UserRegistrationForm(data)
+                if register_form.is_valid():
+                    user = register_form.save()
+                    auth_user = authenticate(username=user.email, password=cd['password'])
+                    auth_login(request, auth_user)
+                    order.user = user
+                else:
+                    self.error_message = register_form
+                    return self.render_to_response({'cart':self.cart, 'form':form, 'login_form':self.login_form, 'error_message':self.error_message})      
+            else:
+                order.user = request.user
+            order.final_price = int(self.cart.get_total_price_after_discount()) * 23000
+            order.save()
+            for item in self.cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            self.cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:request')) 
+        return self.render_to_response({'cart':self.cart, 'form':form, 'login_form':self.login_form, 'error_message':self.error_message})      
+
 
 @staff_member_required
 def admin_order_detail(request, order_id):
