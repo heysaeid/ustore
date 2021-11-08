38e08bf87900462d8b77db7cc09e5485c084dd71
heysaeid <heysaeid92@gmail.com>
2021-11-08 13:03:51 +0330

    Modify newsletter email sending tasks

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -1,5 +1,6 @@
 from celery import task
 from django.core.mail import send_mail
+from django.conf import settings
 from . import models
 
 @task
@@ -7,10 +8,10 @@ def subscribe_complete(id):
     sub = models.Subscribe.objects.get(id=id)
     subject = 'Newsletter Confirmation'
     message = f'<a href="/newsletter/confirm/?email={sub.email}&conf_num={sub.confirm_num}">clicking here to confirm your registration<</a>'
-    send_mail(subject, message, sub.email, ['ustore@info.com'])
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [sub.email])
 
 @task
 def newsletter_complete(subject, message):
     subcribers = models.Subscribe.objects.filter(confirmed=True)
     for sub in subcribers:
-        send_mail(subject, message, 'ustore@info.com', [sub.email])
+        send_mail(subject, message, settings.EMAIL_HOST_USER, [sub.email])
